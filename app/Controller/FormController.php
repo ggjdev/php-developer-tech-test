64cd231d015652439e73dc53869061c4e67ef1c0
ggjdev <168197965+ggjdev@users.noreply.github.com>
2024-04-30 22:25:57 +0100

    Add function to fetch max matches from the ENV
    Attempts to fetch the max number of companies to match from the ENV.
    If after casting to an int it is 0 or less, it defaults to 3.

--- app/Controller/FormController.php
+++ app/Controller/FormController.php
@@ -19,4 +19,15 @@ class FormController extends Controller
             'matchedCompanies'  => $matchedCompanies,
         ]);
     }
+
+    private function getMaxMatchedCompanies(): int
+    {
+        $maxMatchedCompanies = (int) $_ENV['MAX_MATCHED_COMPANIES'];
+
+        if ($maxMatchedCompanies < 1) {
+            $maxMatchedCompanies = 3;
+        }
+
+        return $maxMatchedCompanies;
+    }
 }
