1dac85216c951d8fbf271e8f16881a451676b635
ggjdev <168197965+ggjdev@users.noreply.github.com>
2024-05-01 00:16:11 +0100

    Make `more` link display additional information

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -11,3 +11,27 @@ document.addEventListener('submit', function (e) {
         });
     }
 });
+
+document.addEventListener('click', function (e) {
+    const element = e.target;
+
+    if (element.tagName === 'A' && element.classList.contains('matches__match__more')) {
+        e.preventDefault();
+
+        const parent = element.closest('.matches__match');
+
+        if (parent) {
+            const details = parent.querySelector('.match__match__details');
+
+            if (details) {
+                if (details.style.display === 'none') {
+                    details.style.display = 'block';
+                    element.textContent = 'less';
+                } else {
+                    details.style.display = 'none';
+                    element.textContent = 'more';
+                }
+            }
+        }
+    }
+});
